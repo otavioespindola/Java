ce5596d59ef129f3ad2bdfac2dec5e9639cec6d3
Otavio Espindola <otavioespindola@gmail.com>
2022-03-07 13:47:09 -0300

    Estudos em fundamentos de programação em Java

---
+++ clinica/src/tela/TelaPrincipal.java
@@ -0,0 +1,8 @@
+package tela;
+
+public class TelaPrincipal {
+	//Tudo começa no main
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    	
+    	//Uma sentença termina com um ;
+        System.out.println("Primeiro Programa");
+        /*
+         * Comentário de múltiplas linhas
+         * Funcioan assim
+         */
+        System.out.println("Segundo Programa");
+    }
+}
